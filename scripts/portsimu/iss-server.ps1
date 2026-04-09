1056e580cd7c1ae9877c1e842a194a6dc9097061
Arnold Parada <38769988+xela69@users.noreply.github.com>
2026-04-08 19:45:13 -0700

    Updates 2026-04-08 19:44

--- scripts/portsimu/iss-server.ps1
+++
@@ -1,7 +0,0 @@
-# Install IIS Server#
-
-Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools
-
-# Create simple HTML website #
-
-echo '<!doctype html><html><body><h1>Hello Cloud Gurus!</h1></body></html>' > C:\inetpub\wwwroot\index.html
